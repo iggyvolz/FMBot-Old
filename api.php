221da5ed2c429247d66da61b66429f32aae02949
iggyvolz <iggyvolz@aim.com>
2015-06-09 18:34:18 -0400

    Add PM reading

--- api.php
+++ api.php
@@ -49,9 +49,17 @@ class phpbbRemoteApi
   {
     file_put_contents("posts/$s",serialize(new phpBBPost($this->url,$this->f,$this->t,$s)));
   }
+  public function download_pm($p)
+  {
+    file_put_contents("pms/$p",serialize(new phpBBPM($this->url,$p)));
+  }
   public function get_post($s)
   {
     return unserialize(file_get_contents("posts/$s"));
+  }
+  public function get_pm($p)
+  {
+    return unserialize(file_get_contents("pms/$p"));
   }
   public function update_num_posts()
   {
@@ -63,6 +71,10 @@ class phpbbRemoteApi
     }
     $this->num_posts=trim($nresult);
     return $this->num_posts+0;
+  }
+  public function unread_pms()
+  {
+
   }
   public function create_post($subject,$message)
   {
@@ -102,6 +114,45 @@ class phpBBPost
     $result=preg_replace("~<blockquote(.*?)>(.*)</blockquote>~si","",' '.$result.' ',1);
     $this->author=explode("<",explode("\">",explode("<strong><a href",explode("<p class=\"author\">",$result)[1])[1])[1])[0];
     $this->time=new DateTime(explode(" </p>",explode("</strong> &raquo; ",explode("<p class=\"author\">",$result)[1])[1])[0]);
+    $this->conts=strip_tags(explode("</div>",explode("<div class=\"content\">",$result)[1])[0]);
+  }
+  private function curlrequest($url,$params=NULL)
+  {
+    sleep(3);
+    $handle=curl_init($url);
+    curl_setopt($handle, CURLOPT_COOKIEFILE, phpbbRemoteApi::COOKIE_FILE);
+    curl_setopt($handle, CURLOPT_COOKIEJAR,   phpbbRemoteApi::COOKIE_FILE);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($handle, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($handle, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.6) Gecko/20100625 Firefox/3.6.6 (.NET CLR 3.5.30729)");
+    curl_setopt($handle, CURLOPT_TIMEOUT, round(phpbbRemoteApi::TIMEOUT,0));
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, round(phpbbRemoteApi::TIMEOUT,0));
+    if($params)
+    {
+      curl_setopt($handle, CURLOPT_POST, true);
+      curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($params));
+    }
+    return $handle;
+  }
+}
+class phpBBPM
+{
+  public $p;
+  public $time;
+  public $subject;
+  public $conts;
+  public function __construct($url,$p)
+  {
+    $this->p=$p;
+    $handle=$this->curlrequest(sprintf("%s/ucp.php?i=pm&mode=view&f=0&p=%u",$url,$p));
+    $result=curl_exec($handle);
+    curl_close($handle);
+    //file_put_contents("result.html",$result);
+    $result=preg_replace("~<blockquote(.*?)>(.*)</blockquote>~si","",' '.$result.' ',1);
+    $this->time=new DateTime(trim(explode("<br />",explode("</strong>",explode("<p class=\"author\">",$result)[1])[1])[0]));
+    $this->subject=strip_tags(explode("</h3>",explode("<h3 class=\"first\">",$result)[1])[0]);
     $this->conts=strip_tags(explode("</div>",explode("<div class=\"content\">",$result)[1])[0]);
   }
   private function curlrequest($url,$params=NULL)
