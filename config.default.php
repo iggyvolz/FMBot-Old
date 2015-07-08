7f6b2d8e098651c96288ba7c783e30dbc09acba5
iggyvolz <iggyvolz@aim.com>
2015-07-08 11:36:58 -0400

    Add default config file

---
+++ config.default.php
@@ -0,0 +1,8 @@
+<?php
+define("PHPBB_URL","");
+define("PHPBB_FORUM",0);
+define("PHPBB_TOPIC",0);
+define("PHPBB_USER","");
+define("PHPBB_PASSWORD","");
+define("FMBOT_PLAYERS",serialize(["a","b","c"]));
+
