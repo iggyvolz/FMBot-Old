6396103832c0df32c963f25ae3550d08c006dd9f
iggyvolz <iggyvolz@aim.com>
2015-07-24 13:07:46 -0400

    Add parenthesis

--- config.default.php
+++ config.default.php
@@ -5,4 +5,4 @@ define("PHPBB_TOPIC",0);
 define("PHPBB_USER","");
 define("PHPBB_PASSWORD","");
 define("FMBOT_PLAYERS",serialize(["a","b","c"]));
-define("FMBOT_NUM_LIVE_PLAYERS,0);
+define("FMBOT_NUM_LIVE_PLAYERS",0);
