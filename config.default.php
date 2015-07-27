e5b30bc52a5eba88fffaf0925e7cefacadd91a10
iggyvolz <iggyvolz@aim.com>
2015-07-27 14:13:39 -0400

    Add Jenkins stuff to settings

--- config.default.php
+++ config.default.php
@@ -5,4 +5,4 @@ define("PHPBB_TOPIC",0);
 define("PHPBB_USER","");
 define("PHPBB_PASSWORD","");
 define("FMBOT_PLAYERS",serialize(["a","b","c"]));
-
+define("FMBOT_JENKINS_URL","http://127.0.0.1:8080");
